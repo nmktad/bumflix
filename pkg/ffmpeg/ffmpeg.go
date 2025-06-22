8fa7f9d402c297a5308918f0e1fb5ccf7c14ad83
nmktad <nmktad@gmail.com>
2025-06-22 21:04:54 +0300

    feat: http serve playlist file for streaming

---
+++ pkg/ffmpeg/ffmpeg.go
@@ -0,0 +1,58 @@
+package ffmpeg
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+)
+
+type Variant struct {
+	Name      string
+	Width     int
+	Height    int
+	BitrateK  int
+	OutputDir string
+}
+
+func GenerateHLSVariants(input string, variants []Variant) error {
+	for _, v := range variants {
+		outputPath := fmt.Sprintf("%s/index.m3u8", v.OutputDir)
+
+		err := ffmpeg.Input(input).
+			Filter("scale", ffmpeg.Args{fmt.Sprint(v.Width), fmt.Sprint(v.Height)}).
+			Output(outputPath,
+				ffmpeg.KwArgs{
+					"c:v":                  "libx264",
+					"b:v":                  fmt.Sprintf("%dk", v.BitrateK),
+					"c:a":                  "aac",
+					"hls_time":             6,
+					"hls_playlist_type":    "vod",
+					"hls_segment_filename": fmt.Sprintf("%s/segment_%%03d.ts", v.OutputDir),
+					"f":                    "hls",
+				}).
+			OverWriteOutput().
+			Run()
+		if err != nil {
+			return fmt.Errorf("failed for %s: %w", v.Name, err)
+		}
+	}
+	return nil
+}
+
+func WriteMasterPlaylist(outputDir string, variants []Variant) error {
+	var builder string
+	builder += "#EXTM3U\n"
+
+	for _, v := range variants {
+		line := fmt.Sprintf(
+			"#EXT-X-STREAM-INF:BANDWIDTH=%d,RESOLUTION=%dx%d\n%s/index.m3u8\n",
+			v.BitrateK*1000, v.Width, v.Height, v.Name,
+		)
+		builder += line
+	}
+
+	masterPath := filepath.Join(outputDir, "master.m3u8")
+	return os.WriteFile(masterPath, []byte(builder), 0644)
+}
---
+++ pkg/s3/presigner.go
@@ -0,0 +1,34 @@
+package S3
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type Presigner struct {
+	PresignClient *s3.PresignClient
+}
+
+// GetObject makes a presigned request that can be used to get an object from a bucket.
+// The presigned request is valid for the specified number of seconds.
+func (presigner Presigner) GetObject(
+	ctx context.Context, bucketName string, objectKey string, lifetimeSecs int64,
+) (*v4.PresignedHTTPRequest, error) {
+	request, err := presigner.PresignClient.PresignGetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+	}, func(opts *s3.PresignOptions) {
+		opts.Expires = time.Duration(lifetimeSecs * int64(time.Second))
+	})
+	if err != nil {
+		log.Printf("Couldn't get a presigned request to get %v:%v. Here's why: %v\n",
+			bucketName, objectKey, err)
+	}
+
+	return request, err
+}
